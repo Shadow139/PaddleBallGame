b73b15d4526412a60767b409936a4ec8b95dfee9
Wiktor Manczarski <cloud99_9@hotmail.com>
2014-11-01 11:18:09 +0100

    -paddle

---
+++ src/com/testgame/game/model/Paddle.java
@@ -0,0 +1,43 @@
+package com.testgame.game.model;
+
+import java.awt.*;
+
+/**
+ * Created by Hollow on 01.11.14.
+ */
+public class Paddle {
+    private final static int MOVEMENT_SPEED = 5;
+    private int x,y,width,height,velY;
+    private Rectangle rectangle;
+
+    public Paddle(int height, int x, int y, int width) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        rectangle = new Rectangle(x,y,width,height);
+        velY = 0;
+    }
+
+    public void update(){
+        y += velY;
+        updateRectangle();
+    }
+
+    private void updateRectangle() {
+        rectangle.setBounds(x,y,width,height);
+    }
+
+    public void accelUp(){
+        velY = -MOVEMENT_SPEED;
+    }
+
+
+    public void accelDown(){
+        velY = MOVEMENT_SPEED;
+    }
+
+    public void stop(){
+        velY = 0;
+    }
+}
